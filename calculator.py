1d6251e55850b860fe0565159ef853c5458e1e3e
Chathuki Sandarekha Senarath <73237777+Chathuki@users.noreply.github.com>
2023-08-01 18:48:16 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,25 @@
+def add(x, y):
+  """Adds two numbers."""
+  return x + y
+
+def subtract(x, y):
+  """Subtracts two numbers."""
+  return x - y
+
+def multiply(x, y):
+  """Multiplies two numbers."""
+  return x * y
+
+def divide(x, y):
+  """Divides two numbers."""
+  return x / y
+
+def main():
+  """Prints the results of adding, subtracting, multiplying, and dividing two numbers."""
+  print(add(10, 5))
+  print(subtract(10, 5))
+  print(multiply(10, 5))
+  print(divide(10, 5))
+
+if __name__ == "__main__":
+  main()
